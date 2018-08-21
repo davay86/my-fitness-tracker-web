811957b419410f0531e06e63fb099c65b3336a9b
davidronaldson <davay86@gmail.com>
2018-08-21 21:06:31 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponentComponent } from './components/header-component/header-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponentComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
